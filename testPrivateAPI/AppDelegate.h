f93d916bb60c957a9b7300629301d03840422bbb
donglyu <lydsnm@outlook.com>
2018-06-24 17:01:30 +0800

    commit

---
+++ testPrivateAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPrivateAPI
+//
+//  Created by donglyu on 2018/6/19.
+//  Copyright © 2018年 donglyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testPrivateAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testPrivateAPI
+//
+//  Created by donglyu on 2018/6/19.
+//  Copyright © 2018年 donglyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
